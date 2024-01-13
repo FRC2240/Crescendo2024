f40b9cfcd796cf3c1ab775ec8ee7956fd467913a
Cynosure-Null <cynosure@disroot.org>
2024-01-13 16:46:42 -0700

    fixed auto selection

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -8,11 +8,11 @@
 
 #include <frc/TimedRobot.h>
 #include <frc2/command/CommandPtr.h>
-
 #include "RobotContainer.h"
 
-class Robot : public frc::TimedRobot {
- public:
+class Robot : public frc::TimedRobot
+{
+public:
   void RobotInit() override;
   void RobotPeriodic() override;
   void DisabledInit() override;
@@ -28,7 +28,7 @@ class Robot : public frc::TimedRobot {
   void TestPeriodic() override;
   void TestExit() override;
 
- private:
+private:
   std::optional<frc2::CommandPtr> m_autonomousCommand;
 
   RobotContainer m_container;
