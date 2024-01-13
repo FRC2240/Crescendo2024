a78b0667241bf3a57c7933c805c7767b655b2665
jacksonblackman <endirjacob7238@gmail.com>
2024-01-13 14:10:09 -0700

    Added stuff

--- src/main/cpp/subsystems/Climber.cpp
+++ src/main/cpp/subsystems/Climber.cpp
@@ -4,7 +4,36 @@
 
 #include "subsystems/Climber.h"
 
-Climber::Climber() = default;
+//Climber::Climber() = default;
+ Climber::Climber() {
+    ctre::phoenix6::configs::TalonFXConfiguration hieght_climber_config {};
+    hieght_climber_config.Slot0.kP = 0.1;
+    hieght_climber_config.Slot0.kI = 0.1
+    hieght_climber.GetConfigurator().Apply(hieght_climber_config);
+ }
+   
+   
+   void Climber:: climb (double pos) 
 
+    if m_stick.Y(Climber_up){
+
+          hieght_climber.SetControl(ctre::phoenix6::controls::PostionDutyCycle{units::angle::turn_t{25}}) 
+
+            
+    }
+   if m_stick.X(Climber_down){
+      
+        
+         hieght_climber.SetControl(ctre::phoenix6::controls::PostionDutyCycle{units::angle::turn_t{-25}}) 
+   
+
+   }
+   
+    
 // This method will be called once per scheduler run
 void Climber::Periodic() {}
+  
+
+  
+
+    
--- src/main/include/subsystems/Climber.h
+++ src/main/include/subsystems/Climber.h
@@ -3,19 +3,53 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #pragma once
-
+#include <units/angle.h>
+#include <ctre/phoenix6/TalonFX.hpp>
+#include <frc2/command/CommandPtr.h>
 #include <frc2/command/SubsystemBase.h>
 
 class Climber : public frc2::SubsystemBase {
  public:
-  Climber();
+   Climber();
+  
+  void 
+    
+   
+
 
   /**
    * Will be called periodically whenever the CommandScheduler runs.
    */
-  void Periodic() override;
-
+  void Periodic() override {}
+  
+ 
  private:
-  // Components (e.g. motor controllers and sensors) should generally be
+     enum{
+      up
+      down
+     }
+    
+      m_pos = 0.0;
+      m_rmp = 0.0;
+
+
+  //Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
+   
+     ctre::phoenix6::hardware::TalonFX hieght_climber {0};
+    
+       
+
+    
+    
+
+   
+
+
+ 
+
+
+ 
+
+
 };
