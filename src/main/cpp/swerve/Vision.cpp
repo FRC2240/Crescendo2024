e7d09b98c5f724262c81685eddabf93813ca0bbf
Cynosure-Null <cynosure@disroot.org>
2024-01-27 14:31:52 -0700

    half added pv updates

--- src/main/cpp/swerve/Vision.cpp
+++ src/main/cpp/swerve/Vision.cpp
@@ -1,83 +1,75 @@
 #include "swerve/Vision.h"
+#include "swerve/Drivetrain.h"
 
-Vision::Vision() = default;
+Vision::Vision(std::function<units::degree_t()> get_angle_fn)
+    : get_angle{get_angle_fn} {
 
-std::vector<std::optional<frc::Pose2d>> Vision::get_bot_position()
-{
-    // NOTE: THIS IS TO BE REWRITTEN TO A VECTOR OF ALL CAMERAS, NOT JUST 1
-    auto ll_results = m_limelight->GetNumberArray("botpose_wpiblue", std::vector<double>(6));
-    std::vector<std::optional<frc::Pose2d>> ret;
-    frc::SmartDashboard::PutNumber("X", ll_results[0]);
-    frc::SmartDashboard::PutNumber("Y", ll_results[1]);
-    frc::SmartDashboard::PutNumber("cast", !(ll_results[0] == (int)0));
-    if (!(ll_results[0] == (int)0))
-    {
-        ret.push_back(frc::Pose2d(
-            units::meter_t{ll_results[0]},
-            units::meter_t{ll_results[1]},
-            frc::Rotation2d(units::degree_t{ll_results[5]})));
-    }
-    else
-    {
-        ret.push_back(std::nullopt);
-    }
-    frc::SmartDashboard::PutBoolean("ret", ret[0].has_value());
-    return ret;
-}
+      };
 
-std::optional<units::degree_t> Vision::get_coral_angle()
-{
-    if (m_limelight->GetString("tclass", "ERROR") == "note") // Assuming "note" is the correct key
-    {
-        units::degree_t tx{m_limelight->GetNumber("tx", 0.0)};
-        // Target is valid, return info
+std::vector<std::optional<frc::Pose2d>> Vision::get_bot_position() {
+  auto ll_results = m_fore_limelight->GetNumberArray("botpose_wpiblue",
+                                                     std::vector<double>(6));
 
-        return std::optional<units::degree_t>{tx};
-    }
-    else
-    {
-        return std::nullopt;
-    }
-}
-std::optional<units::degree_t> Vision::get_apriltag_angle()
-{
-    if (frc::DriverStation::GetAlliance())
-    {
-        if (frc::DriverStation::GetAlliance().value() == frc::DriverStation::Alliance::kRed)
-        {
-            // Casted to int to strip off decimals and get rid of errors with equality
-            if ((int)m_limelight->GetNumber("tid", 0.0) == 4)
-            {
-                return units::degree_t{m_limelight->GetNumber("tx", 0.0)};
-            }
-            else
-            {
-                fmt::println("DEBUG: apriltag 4 not found");
-                return std::nullopt;
-            }
+  std::vector<std::optional<frc::Pose2d>> ret;
+
+  for (auto &i : m_photoncam_vec) {
+    auto result = i->GetLatestResult();
+    if (result.MultiTagResult().result.isPresent) {
+      auto pose = m_estimator.Update(result);
+      if (pose) {
+        if (CONSTANTS::IN_THRESHOLD<units::degree_t>(
+                pose.value().estimatedPose.Rotation().ToRotation2d().Degrees(),
+                Drivetrain::getAngle(), 3_deg)) {
         }
-        else if (frc::DriverStation::GetAlliance().value() == frc::DriverStation::Alliance::kBlue)
-        {
-            // Casted to int to strip off decimals and get rid of errors with equality
-            if ((int)m_limelight->GetNumber("tid", 0.0) == 7)
-            {
-                return units::degree_t{m_limelight->GetNumber("tx", 0.0)};
-            }
-            else
-            {
-                fmt::println("DEBUG: apriltag 7 not found");
-                return std::nullopt;
-            }
-        }
+      }
     }
-    else
-    {
-        fmt::println("WARN: Alliance color not set");
-        return std::nullopt;
-    }
+  }
+
+  ret.push_back(frc::Pose2d(units::meter_t{ll_results[0]},
+                            units::meter_t{ll_results[1]},
+                            frc::Rotation2d(units::degree_t{ll_results[5]})));
+
+  return ret;
 }
 
-std::optional<units::degree_t> Vision::get_shooter_angle()
-{
+std::optional<units::degree_t> Vision::get_coral_angle() {
+  if (m_fore_limelight->GetString("tclass", "ERROR") ==
+      "note") // Assuming "note" is the correct key
+  {
+    units::degree_t tx{m_fore_limelight->GetNumber("tx", 0.0)};
+    // Target is valid, return info
+
+    return std::optional<units::degree_t>{tx};
+  } else {
+    return std::nullopt;
+  }
 }
+std::optional<units::degree_t> Vision::get_apriltag_angle() {
+  if (frc::DriverStation::GetAlliance()) {
+    if (frc::DriverStation::GetAlliance().value() ==
+        frc::DriverStation::Alliance::kRed) {
+      // Casted to int to strip off decimals and get rid of errors with equality
+      if ((int)m_aft_limelight->GetNumber("tid", 0.0) == 4) {
+        return units::degree_t{m_aft_limelight->GetNumber("tx", 0.0)};
+      } else {
+        fmt::println("DEBUG: apriltag 4 not found");
+        return std::nullopt;
+      }
+    } else if (frc::DriverStation::GetAlliance().value() ==
+               frc::DriverStation::Alliance::kBlue) {
+      // Casted to int to strip off decimals and get rid of errors with equality
+      if ((int)m_aft_limelight->GetNumber("tid", 0.0) == 7) {
+        return units::degree_t{m_aft_limelight->GetNumber("tx", 0.0)};
+      } else {
+        fmt::println("DEBUG: apriltag 7 not found");
+        return std::nullopt;
+      }
+    }
+  } else {
+    fmt::println("WARN: Alliance color not set");
+    return std::nullopt;
+  }
+}
+
+std::optional<units::degree_t> Vision::get_shooter_angle() {}
 Vision::~Vision() = default;
--- src/main/include/swerve/Vision.h
+++ src/main/include/swerve/Vision.h
@@ -1,53 +1,76 @@
 #pragma once
 #include "Constants.h"
-#include <units/angle.h>
-#include <stdlib.h>
 #include "networktables/NetworkTable.h"
 #include "networktables/NetworkTableEntry.h"
 #include "networktables/NetworkTableValue.h"
-#include <networktables/NetworkTableInstance.h>
-#include <frc/estimator/SwerveDrivePoseEstimator.h>
 #include <frc/DriverStation.h>
+#include <frc/apriltag/AprilTagFieldLayout.h>
+#include <frc/apriltag/AprilTagFields.h>
+#include <frc/estimator/SwerveDrivePoseEstimator.h>
 #include <frc/smartdashboard/SmartDashboard.h>
-
-class Vision
-{
+#include <networktables/NetworkTableInstance.h>
+#include <photon/PhotonCamera.h>
+#include <photon/PhotonPoseEstimator.h>
+#include <stdlib.h>
+#include <units/angle.h>
+class Vision {
 public:
-    /*
-    At the time of writing, Vision must be capable of:
-    1. Translation accuracy within 6in
-    1.a. Doing so no slower than 0.9s
-    2. determining the angle of a note to within 3 degrees
-    3. Determing the distance to a note within 29 inches
-    4. Determing the angle to an apriltag within 2 degrees
-    */
+  /*
+  At the time of writing, Vision must be capable of:
+  1. Translation accuracy within 6in
+  1.a. Doing so no slower than 0.9s
+  2. determining the angle of a note to within 3 degrees
+  3. Determing the distance to a note within 29 inches
+  4. Determing the angle to an apriltag within 2 degrees
+  */
 
-    Vision();
-    ~Vision();
+  Vision(std::function<units::degree_t()> get_angle_fn);
+  ~Vision();
 
-    // Optionals are used liberally in this file due to the uncertain nature of vison.
-    // See https://stackoverflow.com/questions/16860960/how-should-one-use-stdoptional for more info
+  // Optionals are used liberally in this file due to the uncertain nature of
+  // vison. See
+  // https://stackoverflow.com/questions/16860960/how-should-one-use-stdoptional
+  // for more info
 
-    std::optional<units::degree_t> get_shooter_angle();
+  std::optional<units::degree_t> get_shooter_angle();
 
-    // Returns a vector of optionals of camera outputs.
-    // The caller is expected to handle absent data, not the function.
-    std::vector<std::optional<frc::Pose2d>> get_bot_position();
+  // Returns a vector of optionals of camera outputs.
+  // The caller is expected to handle absent data, not the function.
+  std::vector<std::optional<frc::Pose2d>> get_bot_position();
 
-    // Returns the angle to a gp
-    std::optional<units::degree_t> get_coral_angle();
+  // Returns the angle to a gp
+  std::optional<units::degree_t> get_coral_angle();
 
-    // Returns angle to apriltag 4 or 7, depending on alliance color.
-    // Could be modified to work with 3 and 8 as well
-    std::optional<units::degree_t> get_apriltag_angle();
+  // Returns angle to apriltag 4 or 7, depending on alliance color.
+  // Could be modified to work with 3 and 8 as well
+  std::optional<units::degree_t> get_apriltag_angle();
 
 private:
-    std::shared_ptr<nt::NetworkTable> m_limelight =
-        nt::NetworkTableInstance::GetDefault().GetTable("limelight-jank");
+  std::function<units::degree_t()> get_angle;
+  std::shared_ptr<photon::PhotonCamera> m_left_camera_a =
+      std::make_shared<photon::PhotonCamera>("photon-left-a");
+  std::shared_ptr<photon::PhotonCamera> m_left_camera_b =
+      std::make_shared<photon::PhotonCamera>("photon-left-n");
+  std::shared_ptr<photon::PhotonCamera> m_right_camera_a =
+      std::make_shared<photon::PhotonCamera>("photon-right-a");
+  std::shared_ptr<photon::PhotonCamera> m_right_camera_b =
+      std::make_shared<photon::PhotonCamera>("photon-right-b");
 
-    // frc::SwerveDrivePoseEstimator<4> m_estimator {
-    //     m_drivetrain->kinematics,
-    //     m_drivetrain->getAngle(),
+  std::vector<std::shared_ptr<photon::PhotonCamera>> m_photoncam_vec = {
+      m_left_camera_a, m_left_camera_b, m_right_camera_a, m_right_camera_b};
 
-    // }
+  frc::AprilTagFieldLayout layout =
+      frc::LoadAprilTagLayoutField(frc::AprilTagField::k2024Crescendo);
+
+  photon::PhotonPoseEstimator m_estimator{
+      layout, photon::PoseStrategy::MULTI_TAG_PNP_ON_COPROCESSOR,
+      frc::Transform3d(
+          frc::Pose3d(0_m, 0_m, 0_m, frc::Rotation3d(0_rad, 0_rad, 0_rad)),
+          frc::Pose3d(1_m, 1_m, 1_m, frc::Rotation3d(1_rad, 1_rad, 1_rad)))};
+
+  std::shared_ptr<nt::NetworkTable> m_aft_limelight =
+      nt::NetworkTableInstance::GetDefault().GetTable("limelight-aft");
+
+  std::shared_ptr<nt::NetworkTable> m_fore_limelight =
+      nt::NetworkTableInstance::GetDefault().GetTable("limelight-fore");
 };
