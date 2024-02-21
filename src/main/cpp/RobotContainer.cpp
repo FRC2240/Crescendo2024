7e72ead10c0ae3f4735d7f66fbf51651499ef648
musolar <ethan.tonini@outlook.com>
2024-02-21 16:49:46 -0700

    Added binding

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -60,7 +60,8 @@ void RobotContainer::ConfigureBindings()
   m_stick1.Back().OnTrue(m_buddyClimber.ResetCommand());
   
   // Back button
-  //m_stick1.
+  m_stick1.LeftBumper.OnTrue(BackCommand());
+  m_stick1.RightBumper.OnTrue(BackCommand());
 }
 
 frc2::CommandPtr RobotContainer::GetAutonomousCommand()
