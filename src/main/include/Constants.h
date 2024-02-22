f014cbabae1b9c05191a748d5a77cb08e3561f2b
Crosby <crosby.winfield@outlook.com>
2024-02-21 19:29:16 -0700

    changed TOF id

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -56,7 +56,7 @@ namespace CONSTANTS
     constexpr double LOADED_DIST = 350;
     constexpr double LOWER_LOADED_DIST = 350;
     constexpr int TOF_ID = 33;
-    constexpr int LOWER_TOF_ID = 33;
+    constexpr int LOWER_TOF_ID = 34;
     constexpr int BELT_ID = 4;
     constexpr int ANGLE_ID = 3;
     constexpr units::degree_t AUTO_PICKUP_THRESHOLD = 15_deg;
