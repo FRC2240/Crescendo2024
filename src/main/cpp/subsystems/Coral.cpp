910dbd2a9d32beb4f97f3e1cda5c6be022e5fea6
musolar <ethan.tonini@outlook.com>
2024-09-29 11:08:09 -0600

    Fixed "incompatible units" error

--- src/main/cpp/subsystems/Coral.cpp
+++ src/main/cpp/subsystems/Coral.cpp
@@ -8,19 +8,18 @@ frc2::CommandPtr Coral::TrackCommand() {
     return frc2::RunCommand([this] -> void {
         
         //get angle to note from limelight (relative to robot facing angle)
-        units::degree_t rel_x = units::degree_t{m_table->GetNumber("tx", 0.0)};
-        units::degree_t rel_z = units::degree_t{m_table->GetNumber("ty", 0.0)};
+        units::degree_t rel_angle = units::degree_t{m_table->GetNumber("ta", 0.0)};
 
         //compute absolute angle of note
         //maybe try get_absolute_angle()?
-        units::degree_t abs_x = m_drivetrain->getAngle() + rel_x;
+        units::degree_t abs_angle = m_drivetrain->getAngle() + rel_angle;
         
         //get x and y components of forward vector + multiply by coefficient
-        units::meters_per_second_t dx = units::meters_per_second_t{cos(abs_x.value())} * CONSTANTS::CORAL::APPROACH_SPEED;
-        units::meters_per_second_t dy = units::meters_per_second_t{sin(abs_x.value())} * CONSTANTS::CORAL::APPROACH_SPEED;
+        units::meters_per_second_t dx = cos(abs_angle.value()) * CONSTANTS::CORAL::APPROACH_SPEED;
+        units::meters_per_second_t dy = sin(abs_angle.value()) * CONSTANTS::CORAL::APPROACH_SPEED;
 
         //move towards the note
-        m_drivetrain->faceDirection(dx, dy, abs_x, true);
+        m_drivetrain->faceDirection(dx, dy, abs_angle, true);
 
     },{this})
     .WithName("Track");
