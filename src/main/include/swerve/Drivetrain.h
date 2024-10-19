4de53eafee7fa152f8b1a9733233791c743dcb19
musolar <ethan.tonini@outlook.com>
2024-10-19 10:39:57 -0600

    Fixed build error

--- src/main/include/swerve/Drivetrain.h
+++ src/main/include/swerve/Drivetrain.h
@@ -163,7 +163,7 @@ public:
     bool face_direction(units::degree_t tgt, double feedback_device);
 
     bool face_direction(units::degree_t tgt);
-    ctre::phoenix6::hardware::Pigeon2 gyro{CONSTANTS::DRIVE::GYRO_ID, CONSTANTS::DRIVE::CONFIG::GYRO_BUS};
+    ctre::phoenix6::hardware::Pigeon2 gyro{CONSTANTS::DRIVE::GYRO_ID, "swervecan"};
 
 private:
     // ctre::phoenix6::hardware::Pigeon2 gyro{CONSTANTS::DRIVE::GYRO_ID, "rio"};
