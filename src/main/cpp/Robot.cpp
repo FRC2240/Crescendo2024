c06f34fabed6aef959b293e8ca33da9d42683659
cynosure <cynosure@disroot.org>
2024-10-23 16:35:59 -0600

    fixed swerve issue

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -16,7 +16,7 @@ void Robot::RobotPeriodic()
   // auto candle_cmd = m_container.m_candle.get_command(&m_container.m_stick1);
   // candle_cmd.Schedule();
 
-  // m_container.m_odometry.update_from_vision();
+  m_container.m_odometry.update_from_vision();
   m_container.m_odometry.update();
   frc2::CommandScheduler::GetInstance().Run();
 }
